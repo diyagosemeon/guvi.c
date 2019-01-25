a9c08d51303a66367f8c89898897a3c752e57a4c
diyagosemeon <42169900+diyagosemeon@users.noreply.github.com>
2019-01-25 22:24:27 +0530

    Create sample6.c

---
+++ sample6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n;
+printf("enter how many times:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("%d HELLO WORLD \n ");
+}
+return 0;
+}
