86272dd96eee3cad396ff16da1801bf5f77048ac
diyagosemeon <42169900+diyagosemeon@users.noreply.github.com>
2019-01-25 22:06:09 +0530

    Create sample4.c

---
+++ sample4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b.c;
+printf("enter the element");
+scanf("%d %d %d",&a,&b,&c);
+if(a>=b && a>+c)
+{
+printf("%d is largest no",a);
+}
+elseif(b>=a && b>=a)
+{
+printf("%d is largest no",b);
+}
+else
+{
+printf("%d is largest no",c);
+}
+return 0;
+}
+
