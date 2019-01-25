b737c17f968a2e1f9de1138c2e2ba0cca384251a
diyagosemeon <42169900+diyagosemeon@users.noreply.github.com>
2019-01-25 22:42:34 +0530

    Create sample8.c

---
+++ sample8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a=size of (arr[0]);
+int arr={2,3,4,5};
+int sum=0;
+for(int i=0;i<=a;i++)
+{
+sum += arr[1];
+}
+printf("Sum of given array is %d", sum(arr, n)); 
+return 0;
+}
+
